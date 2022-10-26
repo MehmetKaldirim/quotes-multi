ed78722c1aa2ca64adb3d6af76dbb622747b7314
Mehmet <mathdenizi@gmail.com>
2022-10-26 21:24:35 +0200

    NotFounded page and Imperative Navigation added

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,14 @@
+import { useHistory } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return (
-    <div>
-      <p> Here are new Quote </p>
-    </div>
-  );
+  const history = useHistory();
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push("/quotes");
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,25 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Math", text: "Learning React is fun!" },
+  { id: "q2", author: "Mathician", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote Found !</p>;
+  }
   return (
     <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <h> Here are details </h>
       <p>{params.quoteId}</p>
       <Route path="/quotes/:quoteId/comments">
